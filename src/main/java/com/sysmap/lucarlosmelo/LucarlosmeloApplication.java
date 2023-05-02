bc8ce061a8927093368a70d6513e7853d1c731d9
LuCarlosMelo <Luizcapeme@gmail.com>
2023-05-02 15:27:14 -0300

    created project

---
+++ src/main/java/com/sysmap/lucarlosmelo/LucarlosmeloApplication.java
@@ -0,0 +1,13 @@
+package com.sysmap.lucarlosmelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucarlosmeloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucarlosmeloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sysmap/lucarlosmelo/LucarlosmeloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sysmap.lucarlosmelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucarlosmeloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
